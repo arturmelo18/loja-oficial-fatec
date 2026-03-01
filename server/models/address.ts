ba50d22abdad4898cf561566a0a4010fa10a9ec2
Artur Melo <arturmelofavero0504@gmail.com>
2026-03-01 20:56:40 -0300

    feat: update AddressSchema to include neighborhood field

--- server/models/address.ts
+++ server/models/address.ts
@@ -1,29 +1,29 @@
 export const AddressSchema = {
-  name: 'Address',
-  schema: {
-    zipcode: {
-      type: String,
-      required: true,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    street: {
-      type: String,
-      required: true,
-    },
-    number: {
-      type: String,
-      required: true,
-    },
-    complement: {
-      type: String,
-      required: false,
-    },
+  zipcode: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  neighborhood: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+  complement: {
+    type: String,
   },
 }
