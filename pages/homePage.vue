c4b2c3bda056b315d25091e222e6034e14845c4e
Artur Melo <arturmelofavero0504@gmail.com>
2026-03-08 12:52:31 -0300

    style: improve formatting and indentation in homePage.vue

--- pages/homePage.vue
+++ pages/homePage.vue
@@ -7,18 +7,21 @@
       <h1>Produtos</h1>
     </div>
   </div>
-  <!--<ShopCartDrawer />-->
+  <!-- <ShopCartDrawer /> -->
 </template>
 
 <script setup lang="ts">
-import ShopCartDrawer from "~/components/ShopCartDrawer.vue";
+// import ShopCartDrawer from '~/components/ShopCartDrawer.vue'
+definePageMeta({
+  middleware: 'auth',
+})
 </script>
 
 <style lang="css" scoped>
 .nav-bg {
   background-image: url('/imgs/cleversson.jpeg');
   height: 600px;
-  background-size: cover; 
-  background-position: center; 
+  background-size: cover;
+  background-position: center;
 }
 </style>
