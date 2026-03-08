52ae22e94e0ff965294620ba4f86bf6848c4f1c5
Artur Melo <arturmelofavero0504@gmail.com>
2026-03-08 12:48:20 -0300

    style: improve formatting and indentation in NavBar component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,23 +1,30 @@
 <template>
-    <nav class="nav-bar flex">
-    <nuxt-link to="/homePage" class="self-center text-lg min-w-100 ml-2">Loja oficial da Fatec</nuxt-link>
+  <nav class="nav-bar flex">
+    <nuxt-link
+      to="/homePage"
+      class="self-center text-lg min-w-100 ml-2"
+    >Loja oficial da Fatec</nuxt-link>
     <ul class="flex justify-end items-center w-full">
-        <li v-for="card in cards" :key="card.title" class="mr-2 ml-2">
+      <li
+        v-for="card in cards"
+        :key="card.title"
+        class="mr-2 ml-2"
+      >
         <nuxt-link :to="card.to">{{ card.title }}</nuxt-link>
-        </li>
+      </li>
     </ul>
-    </nav>
+  </nav>
 </template>
 
 <script setup lang="ts">
 const cards = [
   {
     title: 'Home',
-    to: '/homePage'
+    to: '/homePage',
   },
   {
     title: 'Área do administrador',
-    to: ''
+    to: '/adminPage',
   },
 ]
 </script>
