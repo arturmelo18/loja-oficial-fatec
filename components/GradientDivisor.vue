45909d2242f7e815be3a5adb76f7d87c08aabf62
Artur Melo <arturmelofavero0504@gmail.com>
2026-03-14 11:38:08 -0300

    feat: create gradient divisor component

---
+++ components/GradientDivisor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gradient-divisor" />
+</template>
+
+<style scoped>
+.gradient-divisor {
+  width: 100%;
+  height: 5px;
+  background: linear-gradient(
+    90deg,
+    #7c1b03 0%,
+    #7a2a16 35%,
+    #b45839 70%,
+    #f0c4a5 100%
+  );
+}
+</style>
