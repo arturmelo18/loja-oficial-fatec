114653f7f90044ca5c0eee804158696f0835c03a
artur.favero <artur.favero@layers.education>
2026-03-16 17:54:39 -0300

    feat: create footer component

---
+++ components/LofFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="content">
+    <GradientDivisor/>
+    <div class="text-center pt-2 pb-2">
+      <span>LOF Todos os Direitos Reservados</span>
+    </div>
+  </footer>
+</template>
+
+<style lang="css" scoped>
+.content {
+  background: #4a0f01;
+  height: 50px;
+}
+</style>
