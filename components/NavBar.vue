22e4d8e7067f993a548cd87a489f58d6aee4924a
Artur Favero <artur.favero@layers.education>
2026-05-30 18:08:37 -0300

    refactor: update navigation links to point to the home page and improve dropdown structure

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,29 +1,43 @@
 <template>
   <nav class="flex space-between">
     <div class="nav-logo">
-      <nuxt-link to="/homePage">Fatecano</nuxt-link>
+      <nuxt-link to="/">Fatecano</nuxt-link>
     </div>
     <ul class="nav-links" id="nav-links-desktop">
-      <li><nuxt-link to="/homePage">Todos os produtos</nuxt-link></li>
+      <li><nuxt-link to="/">Todos os produtos</nuxt-link></li>
     </ul>
-    <el-dropdown>
-      <el-button text type="info">
-        <i class="uil uil-bars"></i>
+    <div>
+      <el-button text type="info" @click="navigateTo('/cartPage')">
+        <i class="uil uil-shopping-cart-alt"></i>
       </el-button>
-      <template #dropdown> 
-        <div class="flex flex-col items-start">
-          <el-dropdown-item class="w-full" @click="navigateTo('/')" v-if="authStore.getUser">
-            Minha Conta
-          </el-dropdown-item>
-          <el-dropdown-item class="w-full" @click="logout" v-if="authStore.getUser">
-            <span>Sair</span>
-          </el-dropdown-item>
-          <el-dropdown-item class="w-full" @click="navigateTo('/adminPage')" v-if="authStore.getUser?.kind == 'admin'">
-            <span>Área do administrador</span>
-          </el-dropdown-item>
-        </div>
-      </template>
-    </el-dropdown>
+      <el-dropdown>
+        <el-button text type="info">
+          <i class="uil uil-bars"></i>
+        </el-button>
+        <template #dropdown> 
+            <div class="flex flex-col items-start" v-if="authStore.getUser">
+              <el-dropdown-item class="w-full" @click="navigateTo('/')">
+                <i class=" uil uil-user"/>
+                <span>Minha Conta</span>
+              </el-dropdown-item>
+              <el-dropdown-item class="w-full" @click="navigateTo('/adminPage')" v-if="authStore.getUser?.kind == 'admin'">
+                <i class="uil uil-setting"></i>
+                <span>Área do administrador</span>
+              </el-dropdown-item>
+              <el-dropdown-item class="w-full" @click="logout">
+                <i class="uil uil-signout"></i>
+                <span>Sair</span>
+              </el-dropdown-item>
+            </div>
+            <div class="flex flex-col items-start" v-else>
+              <el-dropdown-item class="w-full" @click="navigateTo('/loginPage')"">
+                <i class="uil uil-signin"></i>
+                <span>Entrar</span>
+              </el-dropdown-item>
+            </div>
+        </template>
+      </el-dropdown>
+    </div>
   </nav>
 </template>
 
@@ -32,6 +46,7 @@ const authStore = useAuthStore()
 
 const logout = () => {
   authStore.clearUser()
+  ElMessage.success('O usuário saiu da conta com sucesso!')
   navigateTo('/')
 }
 </script>
